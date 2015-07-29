53638d670c3fc571a8da2b9041d98ebe9f957081
Xiang Pan <xiangpan2011@gmail.com>
2015-07-29 15:30:42 -0500

    ds

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir logs
+mkdir js_repository
+openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
 sudo npm install --save express
 sudo npm install --save morgan
 sudo npm install --save cors

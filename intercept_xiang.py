757bfe162e024bc0b8fd3defc2a12db3a0b6b4b3
Xiang Pan <xiangpan2011@gmail.com>
2015-07-29 15:34:38 -0500

    sd

--- intercept_xiang.py
+++ intercept_xiang.py
@@ -1,5 +1,6 @@
 import sys, traceback
 sys.path.append(".")
+sys.path.append("/usr/local/lib/python2.7/site-packages")
 
 from hashlib import sha1
 from bs4 import BeautifulSoup
@@ -9,14 +10,13 @@ from csp_applier import html, template
 from dom_analyzer import DOMAnalyzer
 
 def start(context, argv):
-    if len(argv) != 3:
-        raise ValueError('Usage: -s "intercept.py [http_path] [file_path]"')
+    #if len(argv) != 3:
+    #    raise ValueError('Usage: -s "intercept.py [http_path] [file_path]"')
 
     #context.http_path, context.file_path = argv[1], argv[2]
     context.f = open('./logs/log','w',0)
 
 def response(context, flow):
-<<<<<<< HEAD
     try:
         url = flow.request.url
         context.f.write('receive response: %s\n'%flow.request.url)
@@ -68,47 +68,3 @@ def response(context, flow):
         context.f.write('exception at %s for error: %s\n' %(url, str(e)))
         traceback.print_exc(file=context.f)
 
-
-=======
-    # f1 = open("ctype.txt", "a")
-    # f1.write(flow.response.headers.get_first("content-type", ""))
-    # f1.write('\n')
-    # f1.close()
-    if "text/html" in flow.response.headers.get_first("content-type", ""):
-        with decoded(flow.response):  # Automatically decode gzipped responses.
-            f = open(context.file_path + "/url.text", "a")
-            f.write("------------------" + "\n")
-            f.write(flow.request.url)
-            f.write('\n')
-
-            soup = BeautifulSoup(flow.response.content)
-            try:
-                html_parser = html.HTMLParser(soup)
-                filter_list = []
-        
-                # TODO: If database daemon is running, uncomment these lines
-                # pattern = fetch_template(flow.request.host)
-                # if pattern:
-                #     filter_list = pattern.compare(html_parser)
-        
-                new_content = html.HTMLGenerator(html_parser, filter_list, sha1(flow.request.host).hexdigest(),
-                                                 context.file_path, context.http_path)
-                new_content.write_js()
-                new_content.write_css()
-                new_content.rewrite_html()
-            except:
-                f.write("Unexpected error: " + sys.exc_info()[0])
-
-            flow.response.content = str(new_content.html_parser.soup)
->>>>>>> 5e1980555fca88c8f49e2bf698c6c23bac8d32aa
-
-            f.write('\n')
-            f.close()
-
-def fetch_template(url):
-    db = mongo_driver.MongoDriver()
-    template_string = db.query(sha1(url).hexdigest())
-    if template_string:
-        return template.Template(template_string)
-    else:
-        return None
